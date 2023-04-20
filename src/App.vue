f0dd4bd1e12575aeafa3f3d19b3835396293a145
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-04-20 04:00:40 +0200

    :sparkles: Integrating tailwind.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>test your library here</div>
-  <ShowingMessage :message="'hey'" />
+  <div
+    class="h-screen w-screen flex items-center justify-center bg-gradient-to-br to-cyan-600 from-blue-500"
+  >
+    <Transition
+      appear
+      enter-from-class="opacity-0 scale-75"
+      enter-active-class="transition-all transform duration-1000"
+      enter-to-class="opacity-100 scale-100"
+      leave-from-class="opacity-100 scale-100"
+      leave-active-class="transition-all transform duration-1000"
+      leave-to-class="opacity-0 scale-75"
+      :duration="3000"
+    >
+      <div
+        class="text-center p-8 rounded-full bg-white w-[650px] h-[650px] shadow-lg"
+      >
+        <div>
+          <div class="text-[300px]">🎉</div>
+          <div class="text-2xl text-gray-600 font-thin">
+            Let's build something amazing !
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
-// Import your library here to test.
-import { ShowingMessage } from "./lib";
+// Import your library here.
 </script>
